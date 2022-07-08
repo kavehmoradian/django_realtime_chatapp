8c9d3922546b0a57bc37c0950d8b5021d591d80f
kaveh moradian <kave.moradian@gmail.com>
2022-07-09 00:23:47 +0430

    added home app for index page

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'chat.apps.ChatConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home/home.html")
+
+def about(request):
+    return render(request, "home/about.html")
