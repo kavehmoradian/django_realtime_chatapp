485bd7592fbafaa497b3cb1af6507a639ccf1971
kaveh moradian <kave.moradian@gmail.com>
2022-07-08 22:25:35 +0430

    chat app models created

---
+++ chat/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.db.models import Q
+from accounts.models import User
+
+
+class Chat(models.Model):
+    user1 = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user1_name')
+    user2 = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user2_name')
+    updated_on = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        unique_together = (("user1", "user2"))
+        verbose_name = 'Chat Message'
+        ordering = ['updated_on']
+
+    def __str__(self):
+        return '%s_%s' %(self.user1.username,self.user2.username)
+
+    @property
+    def room_group_name(self):
+        return f'chat_{self.id}'
+
+    @staticmethod
+    def chat_session_exists(user1,user2):
+        return ChatSession.objects.filter(Q(user1=user1, user2=user2) | Q(user1=user2, user2=user1)).first()
+
+    @staticmethod
+    def create_if_not_exists(user1,user2):
+        res = ChatSession.chat_session_exists(user1,user2)
+        return False if res else ChatSession.objects.create(user1=user1,user2=user2)
+
+class Message(models.Model):
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User,
+                            on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat,
+                            on_delete=models.CASCADE,
+                            related_name='user_messages')
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return text[:20]
+
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        Chat.objects.get(id = self.chat_session.id).save()
--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
